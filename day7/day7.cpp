07bbb7b3df5029cd947bb204acc0afbd566611dd
jedwards <jonathan_edwards@mcafee.com>
2021-12-07 06:58:21 -0800

    day 7 part 2

--- day7/day7.cpp
+++ day7/day7.cpp
@@ -13,12 +13,43 @@
 #include <algorithm>
 #include <numeric>
 #include <ranges>
+#include <functional>
 
 using namespace std::literals;
 #include "include/thrower.h"
 
 extern std::istringstream data;
 
+using EnergyCalc = std::function<int(int,int)>;
+
+
+int energyCost(std::vector<int> const &crabs, int targetPosition, EnergyCalc  f)
+{
+    int energy{};
+
+    for(auto crab : crabs)
+    {
+        energy+= f(crab,targetPosition);
+    }
+
+    return energy;
+}
+
+
+void bruteForce(std::vector<int> const &crabs, EnergyCalc f)
+{
+    auto [min,max] = std::ranges::minmax_element(crabs);
+
+    auto minimumEnergy{ std::numeric_limits<int>::max()};
+
+
+    for(auto candidate=*min; candidate<=*max; candidate++)
+    {
+        minimumEnergy = std::min(minimumEnergy, energyCost(crabs,candidate,f));
+    }
+
+    std::cout << minimumEnergy << '\n';
+}
 
 
 int main()
@@ -33,28 +64,36 @@ try
         data.ignore();
     }
 
-    std::ranges::sort(crabs);
-
-    auto median = (  crabs[crabs.size()/2]
-                   + crabs[crabs.size()/2-1]) / 2;
-/*
-
-    double sum = std::accumulate(crabs.begin(), crabs.end(),0);
-
-    auto  mean = sum/crabs.size();
-
-    int  bestPos = std::round(mean);
-
-*/
-    int energy{};
-
-    for(auto crab : crabs)
+    auto part1=[](int a,int b)
     {
-        energy+= std::abs(crab-median);
-    
+        return std::abs(a-b);
+    };
+
+    auto part2=[](int a,int b)
+    {
+        auto distance = std::abs(a-b);
+
+        return (distance*(distance+1))/2;
+    };
+
+
+    bruteForce(crabs,part1);
+    bruteForce(crabs,part2);
+
+    {
+        std::ranges::sort(crabs);
+        auto median = (  crabs[crabs.size()/2] + crabs[crabs.size()/2-1]) / 2;
+
+        std::cout << energyCost(crabs,median,part1) << '\n';
     }
 
-    std::cout << ' ' << energy << '\n';
+    {
+//      int mean    = static_cast<int>(std::round( std::accumulate(crabs.begin(), crabs.end(),0.0) / crabs.size() ));
+        int mean    = static_cast<int>(std::floor( std::accumulate(crabs.begin(), crabs.end(),0.0) / crabs.size() ));
+
+        std::cout << energyCost(crabs,mean,part2) << '\n';
+    }
+
 
     return 0;
 }
